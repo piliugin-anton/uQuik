0c9140ff9e6920d4cf5bd1d288dc2e1d3779517d
Anton Piliugin <anton.piliugin@icloud.com>
2022-05-30 04:59:52 +0500

    Changed handler for Hyper-Express

--- benchmark/Hyper-Express.js
+++ benchmark/Hyper-Express.js
@@ -2,9 +2,7 @@ const HyperExpress = require('hyper-express')
 const webserver = new HyperExpress.Server()
 
 // Create GET route to serve 'Hello World'
-webserver.get('/', (request, response) => {
-  response.send('Hello World')
-})
+webserver.get('/', (request, response) => response.send('Hello World'))
 
 // Activate webserver by calling .listen(port, callback);
 webserver.listen(5002)
